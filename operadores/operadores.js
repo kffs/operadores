53d44acff7e0be003227b477386448545f5e34e7
Kauan Franco <134503958+kffs@users.noreply.github.com>
2023-08-22 08:46:05 -0300

    Add files via upload

---
+++ operadores/operadores.js
@@ -0,0 +1,47 @@
+
+
+function SomaValores(){
+
+var s1 = parseInt(document.getElementById('val1').value);
+var s2 = parseInt(document.getElementById('val2').value);
+var s3 = parseInt(s1) + parseInt(s2);
+
+alert(s3);
+
+
+}
+
+function SubtrairValores(){
+
+    var s1 = parseInt(document.getElementById('val3').value);
+    var s2 = parseInt(document.getElementById('val4').value);
+    var s3 = parseInt(s1) - parseInt(s2) ;
+    alert(s3)
+    
+    }
+
+function MultiplicarValores(){
+
+        var s1 = parseInt(document.getElementById('val3').value);
+        var s2 = parseInt(document.getElementById('val4').value);
+        var s3 = parseInt(s1) * parseInt(s2) ;
+        alert(s3)
+        
+        }
+function DividirValores(){
+
+            var s1 = parseInt(document.getElementById('val3').value);
+            var s2 = parseInt(document.getElementById('val4').value);
+            var s3 = parseInt(s1) / parseInt(s2) ;
+            alert(s3)
+            
+            }
+        
+           
+    
+       
+
+   
+
+
+
